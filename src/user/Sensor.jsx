6f4c95143cd1274c64934ece0e6b46b9bc74fdf0
dannhh <hongdancuibap@gmail.com>
2022-03-28 20:51:03 +0700

    Update data - dashboard

--- src/user/Sensor.jsx
+++ src/user/Sensor.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import '../styles/Sensor.css'
+import axios from "axios";
 import { Line, defaults } from 'react-chartjs-2'
 import temperature from '../img/temperature-icon.png'
 import humidity from '../img/humidity-icon.png'
-import light from '../img/light-icon.png'
+import light_icon from '../img/light-icon.png'
 
 
 import io from 'socket.io-client';
@@ -11,26 +12,55 @@ const sock = io.connect('http://localhost:5000');
 
 function Sensor() {
 
-    const [currentData, setCurrentData] = useState({});
-    const [recentData, setRecentData] = useState({
-        temp: [21, 20, 21, 22, 23, 24],
-        humid: [0.6, 0.5, 0.5, 0.4, 0.4, 0.5].map((h) => { return h; }),
-        light: [300, 350, 300, 420, 470, 420],
-    }); 
+    var [currentData, setCurrentData] = useState({});
+    var [temp, setTemp] = useState([])
+    var [humid, setHumid] = useState([])
+    var [light, setLight] = useState([])
+    var [time, setTime] = useState([])
+    var [currentTime, setCurrentTime] = useState(0)
+    // var count = 0
+
 
     useEffect(() => {
-        sock.on('connect', () => { console.log("Successful"); });
-    
+        sock.on('/current', () => { console.log("Successful"); });
+
         // It would be best to get feed_id ('bbc-test-json') from user info,
         // not hard-coded like this
-        sock.on('bbc-test-json', (data) => { 
-            console.log(data);
-            setCurrentData(JSON.parse(data));
+        axios.get('/history/bbc-test-json').then((response) => {
+            console.log(response.data)
+            setTemp(response.data.temp);
+            setHumid(response.data.humid);
+            setLight(response.data.light);
+            setTime(response.data.time);
+        })
+
+        sock.on('bbc-test-json', (data) => {
+            axios.get('/current').then((response) => {
+                setCurrentData(JSON.parse(data));
+                // count = count + 1;
+                // if (count >= 101) {
+                    setTemp(temp => [...temp, response.data.temp])
+                    setTemp(temp => [...temp.slice(0, 0), ...temp.slice(1, 101)]);
+                    setHumid(humid => [...humid, response.data.humid])
+                    setHumid(humid => [...humid.slice(0, 0), ...humid.slice(1, 101)]);
+                    setLight(light => [...light, response.data.light])
+                    setLight(light => [...light.slice(0, 0), ...light.slice(1, 101)]);
+                    setTime(time => [...time, response.data.time]);
+                    setTime(time => [...time.slice(0, 0), ...time.slice(1, 101)])
+                    setCurrentTime(response.data.time)
+                // }
+                // else {
+                //     setTemp(temp => [...temp, response.data.temp]);
+                //     setHumid(humid => [...humid, response.data.humid]);
+                //     setLight(light => [...light, response.data.light]);
+                //     setTime(time => [...time, response.data.time]);
+                // }
+            })
+
         });
-    
+
         return () => sock.removeAllListeners('bbc-test-json');
     }, [])
-
 
     return (
         <div className="content">
@@ -42,7 +72,7 @@ function Sensor() {
                     <div className="card">
                         <div className="top-info">
                             <div className="icon">
-                                <img src = { temperature } alt="g"/>
+                                <img src={temperature} alt="g" />
                             </div>
                             <div className="value">
                                 <h2>Temperature</h2>
@@ -51,13 +81,13 @@ function Sensor() {
                         </div>
                         <div className="time">
                             <div className="line"></div>
-                            <h2>{new Date().toLocaleString() + ''}</h2>
+                            <h2>{currentTime}</h2>
                         </div>
                     </div>
                     <div className="card">
                         <div className="top-info">
                             <div className="icon">
-                                <img src = { humidity } alt="g"/>
+                                <img src={humidity} alt="g" />
                             </div>
                             <div className="value">
                                 <h2>Humidity</h2>
@@ -66,13 +96,13 @@ function Sensor() {
                         </div>
                         <div className="time">
                             <div className="line"></div>
-                            <h2>{new Date().toLocaleString() + ''}</h2>
+                            <h2>{currentTime}</h2>
                         </div>
                     </div>
                     <div className="card">
                         <div className="top-info">
                             <div className="icon">
-                                <img src = { light } alt="g"/>
+                                <img src={light_icon} alt="g" />
                             </div>
                             <div className="value">
                                 <h2>Light</h2>
@@ -81,19 +111,19 @@ function Sensor() {
                         </div>
                         <div className="time">
                             <div className="line"></div>
-                            <h2>{new Date().toLocaleString() + ''}</h2>
+                            <h2>{currentTime}</h2>
                         </div>
                     </div>
                 </div>
-                
+
                 <div className='db-chart' style={{ marginTop: 50 }}>
                     <Line
                         data={{
-                            labels: ['01:01:01', '01:01:02', '01:01:03', '01:01:04', '01:01:05', '01:01:06'],
+                            labels: time,
                             datasets: [
                                 {
                                     label: 'Temperature',
-                                    data: recentData.temp,
+                                    data: temp,
                                     backgroundColor: [
                                         'rgba(54, 162, 235, 0.2)',
                                         'rgba(75, 192, 192, 0.2)',
@@ -107,7 +137,7 @@ function Sensor() {
                                 },
                                 {
                                     label: 'Humidity',
-                                    data: recentData.humid,
+                                    data: humid,
                                     backgroundColor: [
                                         'rgba(54, 162, 235, 0.2)',
                                         'rgba(75, 192, 192, 0.2)',
@@ -157,11 +187,11 @@ function Sensor() {
                 <div className='db-chart'>
                     <Line
                         data={{
-                            labels: ['01:01:01', '01:01:02', '01:01:03', '01:01:04', '01:01:05', '01:01:06'],
+                            labels: time,
                             datasets: [
                                 {
                                     label: 'Light',
-                                    data: recentData.light,
+                                    data: light,
                                     backgroundColor: [
                                         'rgba(54, 162, 235, 0.2)',
                                         'rgba(75, 192, 192, 0.2)',
